691da8a9782968f034ed08e8677a5d4c0780b92a
Rudra Sharma <rudrasharma8271@gmail.com>
2026-06-20 11:53:32 +0530

    update

--- Js/About.js
+++ Js/About.js
@@ -1,28 +1,24 @@
 const aboutSection = document.querySelector("#about");
 const aboutCard = document.querySelector(".about-card");
+const aboutImageBox = document.querySelector(".about-image-box");
 
 const aboutObserver = new IntersectionObserver((entries) => {
 
     entries.forEach(entry => {
 
         if(entry.isIntersecting){
-
-            aboutCard.classList.remove("show");
-
-            void aboutCard.offsetWidth;
-
             aboutCard.classList.add("show");
-
-        }else{
-
+            aboutImageBox.classList.add("show");
+        } 
+        else {
             aboutCard.classList.remove("show");
-
+            aboutImageBox.classList.remove("show");
         }
 
     });
 
-},{
-    threshold:0.4
+}, {
+    threshold: 0.4
 });
 
 aboutObserver.observe(aboutSection);
